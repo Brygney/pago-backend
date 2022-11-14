ac773b81ba503875f1e08b786f429f401eb0ddf6
Camila Garcia <020200359j@uandina.edu.pe>
2022-11-14 16:56:42 -0500

    Pay

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = require('./pago.router');
+const pagoRouter = require('./pago.router');
+
+function routerApi(app){
+   const routerv1 = express.Router();
+   app.use('/api/v1',routerv1);
+   routerv1.use('/pago',pagoRouter);
+  }
+
+module.exports  = routerApi;
